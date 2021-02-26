b4006d354549f2f849d0a0437797b5da32c238e5
Leandro <thesquallrock@gmail.com>
2021-02-26 15:37:07 +0100

    CSS header - footer

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+  <link rel="stylesheet" href="css/style.css">
+  <title>Document</title>
+</head>
+<body>
+  <header>
+    <div class="top">
+
+      <div>
+        <img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="Logo">
+        <span>Privacy e termini</span>
+      </div>
+
+      <div>
+        <i class="fas fa-th"></i>
+        <i class="fas fa-user-circle"></i>
+      </div>
+
+    </div>
+
+    <nav class="bottom">
+      <ul>
+        <li>Introduzione</li>
+        <li>Norme sulla privacy</li>
+        <li>Termini di servizio</li>
+        <li>Tecnologie</li>
+        <li>Domande frequenti</li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <div class="container">
+
+    </div>
+  </main>
+
+  <footer>
+    <div class="container">
+      <ul>
+        <li>Google</li>
+        <li>Tutto su Google</li>
+        <li>Privacy</li>
+        <li>Termini</li>
+      </ul>
+
+      <div>
+        <i class="fas fa-comment-alt"></i>
+        <select>
+          <option value="Italiano">Italiano</option>
+          <option value="English">English</option>
+        </select>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
